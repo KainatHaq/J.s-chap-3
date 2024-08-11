117a3645fe88810d33bab4e1e19c68ab16712ed4
KainatHaq <kainathaq52@gmail.com>
2024-08-11 16:17:27 +0500

    Add files via upload

---
+++ javascript/scipt.js
@@ -0,0 +1,16 @@
+//Chapter 3
+//Q#1
+//var age = 15;
+//alert("I am 15 years old");
+
+//Q#2
+//var visit = 14;
+//alert("You have visited this site 14 times");
+
+//Q#3
+//var birthday =  1990;
+//document.write("My birth year is" +"" +birthday);
+
+//Q#4
+var content = '<h3>John Doe</h3> ordered <h3>5 T-shirt(s)</h3> on XYZ clothing store';
+document.write(content);
